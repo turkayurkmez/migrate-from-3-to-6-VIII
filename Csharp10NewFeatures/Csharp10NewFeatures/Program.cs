3b28c4017a65c74053ca6951dbc95087d6782248
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-04-04 15:38:03 +0300

    Exception Guards explained

--- Csharp10NewFeatures/Csharp10NewFeatures/Program.cs
+++ Csharp10NewFeatures/Csharp10NewFeatures/Program.cs
@@ -2,7 +2,9 @@
 
 #region top-level statement ve global (implicitly) usings
 using Csharp10NewFeatures;
+using Csharp10NewFeatures.Services;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 void showSomething(string something)
 {
@@ -52,7 +54,6 @@ var result = object (bool isOk) => isOk ? 1 : "İşlem hatalı";
 
 #endregion
 
-
 #region Record ve Record Structure kavramları
 
 ProductRecord productRecord1 = new ProductRecord(1, "Çamaşır Makinası", 30000);
@@ -80,5 +81,91 @@ Console.WriteLine($"Adres içinde yer alıyor mu: {isFind}");
 #endregion
 
 
+#region Karmaşık Tanımlama, deconstructions ve elbette bunların Tuple'a yansıması:
 
 
+Tuple<int, int> bolmeIslemi(int a, int b)
+{
+    //kalan = a % b;
+    var tuple = Tuple.Create(a / b, a % b);
+    return tuple;
+}
+
+(int sonuc, int kalan) tupleOlmadanBolmeIslemi(int a, int b)
+{
+    //kalan = a % b;
+    return (a / b, a % b);
+}
+
+
+var value = bolmeIslemi(13, 5);
+Console.WriteLine(value.Item2);
+
+int num1, num2;
+(num1, num2) = (-5, 4);
+(string name, string lastName, int age) = ("Türkay", "Ürkmez", 44);
+(num1, bool olurMu) = (92, false);
+(int bolumSonucu, int kalan) = bolmeIslemi(13, 5);
+(int bolumSonucu2, int kalan2) = tupleOlmadanBolmeIslemi(13, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endregion
+
+#region Caller Argument Expression geldi. Hayırlı olsun :)
+
+void ConditionChecker(bool condition, [CallerArgumentExpression(nameof(condition))] string? message = null)
+{
+    //Console.WriteLine($"{message} koşulunun sonucu: {condition}");
+    if (!condition)
+    {
+        throw new ArgumentException($"{message} koşulunun sonucu: {condition}");
+    }
+
+}
+(int a1, int b1) = (5, 3);
+ConditionChecker(a1 > b1);
+ConditionChecker(3 > 5);
+string company = "Halkbank";
+ConditionChecker(company.StartsWith("H"));
+
+
+#endregion
+
+#region Exception Guards
+void saveProduct(Product product)
+{
+    ArgumentNullException.ThrowIfNull(nameof(product));
+
+}
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+class BolmeSonucu
+{
+    public int Sonuc { get; set; }
+    public int Kalan { get; set; }
+}
