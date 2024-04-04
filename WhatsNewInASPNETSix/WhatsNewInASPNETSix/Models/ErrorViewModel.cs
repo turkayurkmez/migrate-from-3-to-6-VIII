4d6a5c7a127f9deee1bd377b7aaba85ea0dba5b1
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-04-04 16:00:43 +0300

    what's new in aspdotnet six

---
+++ WhatsNewInASPNETSix/WhatsNewInASPNETSix/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhatsNewInASPNETSix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WhatsNewInASPNETSix/WhatsNewInASPNETSix/Program.cs
@@ -0,0 +1,35 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.Logger.LogInformation($"Uygulama adı: {builder.Environment.ApplicationName}");
+app.Logger.LogInformation($"Web kök dizin: {builder.Environment.WebRootPath}");
+app.Logger.LogInformation($"Kod kök dizin: {builder.Environment.ContentRootPath}");
+app.Logger.LogInformation($"Ortam Adı: {builder.Environment.EnvironmentName}");
+
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
